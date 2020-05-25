5d11a34743fe9fcaf458b45319f544edd9ab605b
dima <amid.vonavi@gmail.com>
2020-05-26 00:30:04 +0300

    fixed signup

--- delivery24/accounts/urls.py
+++ delivery24/accounts/urls.py
@@ -8,7 +8,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    #path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='accounts/form.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('changepwd/', PasswordChangeView.as_view(template_name='accounts/changepwd.html',
--- delivery24/accounts/views.py
+++ delivery24/accounts/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.urls import path, reverse_lazy
-from django.views.generic.edit import FormView
 from .forms import SignUpForm
 
 
-class SignUpView(FormView):
-    template_name = 'accounts/form.html'
-    form_class = SignUpForm
-    success_url = reverse_lazy('core:index')
+# class SignUpView(FormView):
+#     template_name = 'accounts/form.html'
+#     form_class = SignUpForm
+#     success_url = reverse_lazy('core:index')
+#
+#     # def form_valid(self, form):
+#     #     # This method is called when valid form data has been POSTed.
+#     #     # It should return an HttpResponse.
+#     #     #form.send_email()
+#     #     print('++++++++++++++++++++++++++++++++++++++++++')
+#     #     return super().form_valid(form)
 
-    # def form_valid(self, form):
-    #     # This method is called when valid form data has been POSTed.
-    #     # It should return an HttpResponse.
-    #     #form.send_email()
-    #     print('++++++++++++++++++++++++++++++++++++++++++')
-    #     return super().form_valid(form)
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(reverse_lazy('core:index'))
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/form.html', {'form': form})
