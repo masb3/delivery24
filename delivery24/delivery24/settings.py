ec2a74ad6bdbf40940b7acff5b57c941452571d1
dima <amid.vonavi@gmail.com>
2020-05-25 22:18:55 +0300

    login logout redirect to index

--- delivery24/delivery24/settings.py
+++ delivery24/delivery24/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,3 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_REDIRECT_URL = reverse_lazy('core:index')
+LOGOUT_REDIRECT_URL = reverse_lazy('core:index')
+LOGIN_URL = reverse_lazy('accounts:login')
+LOGOUT_URL = reverse_lazy('accounts:logout')
+
