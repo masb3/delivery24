934ab2a0ded10d24aee6032f91c48e28abb4fb44
dima <amid.vonavi@gmail.com>
2020-06-01 09:51:58 +0300

    implemented password reset

--- delivery24/accounts/urls.py
+++ delivery24/accounts/urls.py
@@ -16,26 +16,25 @@ urlpatterns = [
                                                   success_url=reverse_lazy('core:index')), name='changepwd'),
 
     path('account_activation_sent/', views.account_activation_sent, name='account_activation_sent'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate')
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
-    # TODO: pwd reset
-    # path('reset/', PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done'),
-    #                                          template_name='accounts/password_reset.html',
-    #                                          email_template_name='accounts/password_reset_email.html',
-    #                                          subject_template_name='accounts/password_reset_subject.txt'),
-    #      name='password_reset'),
-    #
-    # path('reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
-    #      name='password_reset_done'),
-    #
-    # path('reset/<uidb64>/<token>/',
-    #      PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete'),
-    #                                       template_name='accounts/password_reset_confirm.html'),
-    #      name='password_reset_confirm'),
-    #
-    # path('reset/complete/', PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
-    #      name='password_reset_complete'),
-    #
+    path('reset/', PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done'),
+                                             template_name='accounts/password_reset_form.html',
+                                             email_template_name='accounts/password_reset_email.html',
+                                             subject_template_name='accounts/password_reset_subject.txt'),
+         name='password_reset'),
+
+    path('reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete'),
+                                          template_name='accounts/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('reset/complete/', PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
+
     # TODO: profile
     # path('profile/', views.profile, name='profile'),
 ]
