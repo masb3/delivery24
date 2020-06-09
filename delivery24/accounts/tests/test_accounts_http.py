bb78c413afff5e5bad773b0e52082b246d417787
dima <amid.vonavi@gmail.com>
2020-06-10 01:24:30 +0300

    added signup tests

--- delivery24/accounts/tests/test_accounts_http.py
+++ delivery24/accounts/tests/test_accounts_http.py
@@ -50,14 +50,33 @@ class TestViews:
 
 
 @pytest.mark.django_db
-class TestLogin(TestCase):
-    def setUp(self):
-        self.credentials = {
-            'email': 'testuser@testuser.ee',
-            'password': 'socPet1778Zx+*'}
-        User.objects.create_user(**self.credentials)
+class TestSignUp:
+    url = reverse('accounts:signup')
+    data = {
+        'email': 'john@mail.ee',
+        'first_name': 'john',
+        'last_name': 'doe',
+        'ik': 12345,
+        'phone_0': 55478,
+        'phone_1': 371,
+        'car_model': 'vw',
+        'car_carrying': 1000,
+        'car_number': '123abc',
+        'payment': 1,
+        'password1': 'abcdef123456',
+        'password2': 'abcdef123456'
+    }
 
-    def test_login(self):
-        response = self.client.post('/accounts/login/', self.credentials, follow=True)
-        assert response.status_code == 200
-        self.assertTrue(response.context['user'].is_authenticated)
+    def test_empty_form(self, client):
+        resp = client.post(self.url, {})
+        assert resp.status_code == 200
+
+    def test_correct_signup(self, client, mailoutbox):
+        resp = client.post(self.url, self.data)
+        assert resp.status_code == HttpResponseRedirect.status_code
+        assert resp.url == reverse('accounts:account_activation_sent')
+        assert len(mailoutbox) == 1
+        mail = mailoutbox[0]
+        subject = 'Activate Your delivery24.ee Account'
+        assert mail.subject == subject
+        assert mail.to == [self.data['email']]
