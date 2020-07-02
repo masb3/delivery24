85cf5829e59ed8f113a941410da3e0f60e95b55c
dima <amid.vonavi@gmail.com>
2020-07-03 00:41:24 +0300

    code cleanup

--- delivery24/core/models.py
+++ delivery24/core/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.db import models
 from django.conf import settings
-from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
+from django.core.validators import MinLengthValidator
 from django.utils.translation import ugettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
