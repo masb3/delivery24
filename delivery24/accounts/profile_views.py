d45bf69f3b8065fd73109be63f072ff231c9af2a
dima <amid.vonavi@gmail.com>
2020-08-25 01:54:24 +0300

    profile change save data

--- delivery24/accounts/profile_views.py
+++ delivery24/accounts/profile_views.py
@@ -26,6 +26,23 @@ class ProfileChange(LoginRequiredMixin, FormView):
     login_required = True
     template_name = "accounts/profile/profile_change.html"
     form_class = ChangeProfileForm
+    success_url = reverse_lazy('accounts:profile')
+
+    def form_valid(self, form):
+        self.request.user.first_name = form.cleaned_data.get('first_name')
+        self.request.user.last_name = form.cleaned_data.get('last_name')
+        self.request.user.phone = form.cleaned_data.get('phone')
+        self.request.user.car_model = form.cleaned_data.get('car_model')
+        self.request.user.car_number = form.cleaned_data.get('car_number').replace(' ', '').upper()
+        self.request.user.car_carrying = form.cleaned_data.get('car_carrying')
+        self.request.user.movers_num = form.cleaned_data.get('movers_num')
+        self.request.user.payment = form.cleaned_data.get('payment')
+
+        self.request.user.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
 
 
 class CustomPasswordChangeView(PasswordChangeView):
