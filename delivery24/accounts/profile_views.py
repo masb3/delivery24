c71b525d4679abd3dc7b67a68715724f2ff33176
dima <amid.vonavi@gmail.com>
2020-08-26 00:10:26 +0300

    prefill user change profile form

--- delivery24/accounts/profile_views.py
+++ delivery24/accounts/profile_views.py
@@ -28,6 +28,20 @@ class ProfileChange(LoginRequiredMixin, FormView):
     form_class = ChangeProfileForm
     success_url = reverse_lazy('accounts:profile')
 
+    def get_initial(self):
+        """ Prefill form with user data """
+        initial = super(ProfileChange, self).get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        initial['phone'] = self.request.user.phone
+        initial['car_model'] = self.request.user.car_model
+        initial['car_number'] = self.request.user.car_number
+        initial['car_carrying'] = self.request.user.car_carrying
+        initial['movers_num'] = self.request.user.movers_num
+        initial['payment'] = self.request.user.payment
+
+        return initial
+
     def form_valid(self, form):
         self.request.user.first_name = form.cleaned_data.get('first_name')
         self.request.user.last_name = form.cleaned_data.get('last_name')
